a7671b5c4274ca3fbb1c3a5e254a4c2eb0637e70
Daemuss <alfiemondejar1@outlook.com>
2020-05-14 08:57:28 +0200

    Made a small start for implementing Sqlite

--- src/BarApplication/Main.java
+++ src/BarApplication/Main.java
@@ -5,6 +5,8 @@ import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
+import java.sql.*;
+
 public class Main extends Application {
 
     @Override
@@ -14,6 +16,17 @@ public class Main extends Application {
         primaryStage.setTitle("Bar application");
         primaryStage.setScene(new Scene(root, 640, 480));
         primaryStage.show();
+
+        Connection c = null;
+
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:test.db");
+        } catch ( Exception e ) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+        }
+        System.out.println("Opened database successfully");
     }
 
     public static void main(String[] args) {
