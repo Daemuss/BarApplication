548a674ddacad14beab137eb160d6b48ae291c71
Daemuss <alfiemondejar1@outlook.com>
2020-05-29 08:56:56 +0200

    Changes in BartenderPane

--- src/BarApplication/BartenderPane.java
+++ src/BarApplication/BartenderPane.java
@@ -23,6 +23,8 @@ public class BartenderPane {
 
         Drink beer = new Drink("Beer", 2.00, 2);
         Drink tequila = new Drink("Tequila", 2.00, 5);
+        Drink cola = new Drink("Cola", 2.00, 2);
+        Drink water = new Drink("Water", 2.00, 5);
 
         Order order = new Order(2, false, false);
         order.addDrink(beer);
@@ -30,8 +32,16 @@ public class BartenderPane {
         Order secondOrder = new Order(5, false, false);
         secondOrder.addDrink(tequila);
 
+        Order thirdOrder = new Order(1, false, false);
+        thirdOrder.addDrink(cola);
+
+        Order fourthOrder = new Order(4, false, false);
+        fourthOrder.addDrink(water);
+
         orders.add(order);
         orders.add(secondOrder);
+        orders.add(thirdOrder);
+        orders.add(fourthOrder);
     }
 
     private void showOrders(GridPane p)
@@ -51,10 +61,10 @@ public class BartenderPane {
                     {
                         drinkName = new Label(drink.getDrinkName());
                         amount = new Label(String.valueOf(drink.getAmount()));
-                    }
 
-                    p.add(drinkName, 0, i);
-                    p.add(amount, 1, i);
+                        p.add(drinkName, 0, i);
+                        p.add(amount, 1, i);
+                    }
                     p.add(checboxReadyToServe, 2, i);
 
                     checkboxClickEvent(i, p);
@@ -70,6 +80,8 @@ public class BartenderPane {
     private void triggerRefresh(GridPane p)
     {
         ArrayList<Order> updatedOrders = orders;
+
+        p.getChildren().clear();
 
         if(updatedOrders.size() > 0)
         {
@@ -89,13 +101,14 @@ public class BartenderPane {
                         amount = new Label(String.valueOf(drink.getAmount()));
                     }
 
-                    p.add(drinkName, 0, i + 1);
-                    p.add(amount, 1, i + 1);
-                    p.add(checboxReadyToServe, 2, i + 1);
+                    p.add(drinkName, 0, i);
+                    p.add(amount, 1, i);
+                    p.add(checboxReadyToServe, 2, i);
+
+                    checkboxClickEvent(i, p);
                 }
             }
         }
-
     }
 
     private void checkboxClickEvent(int i, GridPane p)
